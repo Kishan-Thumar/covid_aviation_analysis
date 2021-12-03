c8d5e6665fc7defd0d0d1d20ffa67725fb20e313
Dhruv Patel <dnp5@sfu.ca>
2021-12-02 19:36:30 -0800

    Update transform_delay_data.py
    - Adding UDF code to get_diff_time
    - Adding UDF code get appropriate carrier_name.

--- transform_delay_data.py
+++ transform_delay_data.py
@@ -5,11 +5,15 @@ from pyspark.sql import functions
 from pyspark.sql import types
 from pyspark.sql.functions import avg
 from pyspark.sql.functions import asc
-from pyspark.sql.functions import regexp_extract
 from pyspark.sql.functions import col
 from pyspark.sql.functions import split
 from pyspark.sql.functions import when
 from pyspark.sql.functions import round, concat, lit
+from pyspark.sql.functions import udf, array
+import re
+import datetime
+from pyspark.sql.types import ArrayType, StringType, IntegerType
+
 
 
 """ 
@@ -19,9 +23,33 @@ from pyspark.sql.functions import round, concat, lit
 	3. Removing State Name Abbreviation from ORIGIN_CITY_NAME and DEST_CITY_NAME. Eg: Denver, CO => Denver.
 	4. Dropping ORIGIN_STATE_ABR, DEST_STATE_ABR, ORIGIN_STATE_FIPS and DEST_STATE_FIPS.
 	5. Dropping DEP_DELAY, ARR_DELAY, ARR_DELAY_NEW and DEP_DELAY_NEW because it can be calculate from other fields. 
-	6. Converting those columns [CARRIER_DELAY | WEATHER_DELAY | NAS_DELAY | SECURITY_DELAY | LATE_AIRCRAFT_DELAY ] to [DELAY_CODE | DELAY_PERC | TOTAL_DELAY] 
+	6. Converting those columns [CARRIER_DELAY (1) | WEATHER_DELAY (2) | NAS_DELAY (3) | SECURITY_DELAY (4) | LATE_AIRCRAFT_DELAY (5) ] to [DELAY_CODE | DELAY_PERC | TOTAL_DELAY] 
 	   for effectively storing the data.
+	7. 
 """
+get_time = udf(lambda z,y: get_time_diff(z,y),StringType())
+
+def get_time_diff(a,b):
+	a = str(a)
+	a = a.rjust(4,"0")
+	b = str(b)
+	b = b.rjust(4,"0")
+	t1 = datetime.datetime.strptime(a, "%H%M")
+	t2 = datetime.datetime.strptime(b, "%H%M")
+	t = t2-t1
+	return t.total_seconds()/60
+
+get_carrier_name = udf(lambda z: processed_file_name(z),StringType())
+
+def processed_file_name(flight):
+    fka_re = " fka | f/k/a "
+    dba_re = " dba | d/b/a "
+    if ("fka" in flight) or ("f/k/a" in flight):
+        return re.split(fka_re, flight)[0]
+    elif ("dba" in flight) or ("d/b/a" in flight):
+        return re.split(dba_re, flight)[-1] 
+    else:
+    	return flight 
 
 def main(inputs,inputs1):
 	delay_df = spark.read.csv(inputs,inferSchema = True, header = True)
@@ -29,35 +57,27 @@ def main(inputs,inputs1):
 
 	delay_df = delay_df.na.fill(value=0)
 
-	drop_columns = ["YEAR","MONTH","ORIGIN_STATE_ABR", "DEST_STATE_ABR", "ORIGIN_STATE_FIPS","DEST_STATE_FIPS","DEP_DELAY","DEP_DELAY","ARR_DELAY","ARR_DELAY_NEW","DEP_DELAY_NEW","CANCELLED","CANCELLATION_CODE","_c38"]
+	drop_columns = ["FL_DATE","ORIGIN_STATE_ABR", "DEST_STATE_ABR", "ORIGIN_STATE_FIPS","DEST_STATE_FIPS","DEP_DELAY","ARR_DELAY","ARR_DELAY_NEW","DEP_DELAY_NEW","CANCELLED","CANCELLATION_CODE","_c38"]
 	delay_df = delay_df.drop(*drop_columns)
 
 	delay_df = delay_df.join(l_country_lookup_df,delay_df.OP_UNIQUE_CARRIER == l_country_lookup_df.Code,"left")
 
-	delay_df = delay_df.drop("OP_UNIQUE_CARRIER","Code").withColumnRenamed("Description","OP_UNIQUE_CARRIER")
+	delay_df = delay_df.drop("Code").withColumnRenamed("Description","UNIQUE_CARRIER_NAME")
 
-	delay_df = delay_df.withColumn("ORIGIN_CITY_NAME",split(delay_df['ORIGIN_CITY_NAME'], ',').getItem(0)).withColumn("DEST_CITY_NAME",split(delay_df['DEST_CITY_NAME'], ',').getItem(0)).withColumn("TOTAL_DELAY",col("CARRIER_DELAY")+col("WEATHER_DELAY")+col("NAS_DELAY")+col("SECURITY_DELAY")+col("LATE_AIRCRAFT_DELAY"))
+	delay_df = delay_df.withColumn("ORIGIN_CITY_NAME",split(delay_df['ORIGIN_CITY_NAME'],',').getItem(0)).withColumn("DEST_CITY_NAME",split(delay_df['DEST_CITY_NAME'], ',').getItem(0)).withColumn("DEP_DELAY",get_time(col("DEP_TIME"),col("CRS_DEP_TIME"))).withColumn("ARR_DELAY",get_time(col("ARR_TIME"),col("CRS_ARR_TIME")))
 
-	delay_df = delay_df.withColumn("CARRIER_DELAY_PREC",when(col("CARRIER_DELAY") > 0, round(col("CARRIER_DELAY")/col("TOTAL_DELAY"))).otherwise(col("CARRIER_DELAY"))).withColumn("WEATHER_DELAY_PREC",when(col("WEATHER_DELAY") > 0, round(col("WEATHER_DELAY")/col("TOTAL_DELAY"))).otherwise(col("WEATHER_DELAY"))).withColumn("NAS_DELAY_PREC",when(col("NAS_DELAY") > 0, round(col("NAS_DELAY")/col("TOTAL_DELAY"))).otherwise(col("NAS_DELAY"))).withColumn("SECURITY_DELAY_PREC",when(col("SECURITY_DELAY") > 0, round(col("SECURITY_DELAY")/col("TOTAL_DELAY"))).otherwise(col("SECURITY_DELAY"))).withColumn("LATE_AIRCRAFT_DELAY_PREC",when(col("LATE_AIRCRAFT_DELAY") > 0, round(col("LATE_AIRCRAFT_DELAY")/col("TOTAL_DELAY"))).otherwise(col("LATE_AIRCRAFT_DELAY")))
+	group_by_cols = ["OP_UNIQUE_CARRIER","MONTH","YEAR","DEST_STATE_NM","ORIGIN_STATE_NM","UNIQUE_CARRIER_NAME"]
+	
+	select_cols = ["OP_UNIQUE_CARRIER","UNIQUE_CARRIER_NAME","MONTH","YEAR","CARRIER_DELAY","WEATHER_DELAY", "NAS_DELAY", "SECURITY_DELAY", "LATE_AIRCRAFT_DELAY","ARR_DELAY","DEP_DELAY","DISTANCE","DEST_STATE_NM","ORIGIN_STATE_NM"]
 
-	delay_df = delay_df.withColumn("DELAY_PERC",concat(col("CARRIER_DELAY_PREC"),lit(","),col("WEATHER_DELAY_PREC"),lit(","),col("NAS_DELAY_PREC"),lit(","),col("SECURITY_DELAY_PREC"),lit(","),col("LATE_AIRCRAFT_DELAY_PREC")))
+	delay_df = delay_df.groupBy(*group_by_cols).agg(functions.avg(delay_df['DISTANCE']).alias("DISTANCE"),functions.avg(delay_df['CARRIER_DELAY']).alias("CARRIER_DELAY"),functions.avg(delay_df['WEATHER_DELAY']).alias("WEATHER_DELAY"),functions.avg(delay_df['NAS_DELAY']).alias("NAS_DELAY"),functions.avg(delay_df['SECURITY_DELAY']).alias("SECURITY_DELAY"),functions.avg(delay_df['LATE_AIRCRAFT_DELAY']).alias("LATE_AIRCRAFT_DELAY"),functions.avg(delay_df['DEP_DELAY']).alias("DEP_DELAY"),functions.avg(delay_df['ARR_DELAY']).alias("ARR_DELAY")).select(*select_cols)
 
-	delay_df = delay_df.withColumn("DELAY_CODE",lit("NO_DELAY"))
+	delay_df = delay_df.withColumn("UNIQUE_CARRIER_NAME",get_carrier_name("UNIQUE_CARRIER_NAME"))
 
-	delay_df = delay_df.withColumn("DELAY_CODE", when(col("CARRIER_DELAY_PREC")>0,concat(col("DELAY_CODE"),lit("CARRIER_DELAY,")))
-		.when(col("WEATHER_DELAY_PREC")>0,concat(col("DELAY_CODE"),lit("WEATHER_DELAY,")))
-		.when(col("NAS_DELAY_PREC")>0, concat(col("DELAY_CODE"),lit("NAS_DELAY,")))
-		.when(col("SECURITY_DELAY_PREC")>0,concat(col("DELAY_CODE"),lit("SECURITY_DELAY,")))
-		.when(col("LATE_AIRCRAFT_DELAY_PREC")>0,concat(col("DELAY_CODE"),lit("LATE_AIRCRAFT_DELAY,"))))
-
-	print(delay_df.select('DELAY_CODE').distinct().collect())
-
-	# delay_df.show()
+	delay_df.show()
 
 if __name__ == '__main__':
 	inputs = sys.argv[1] # "/Users/dhruv/Downloads/636076285_T_ONTIME_REPORTING 2.csv"
 	inputs1 = sys.argv[2] # "/Users/dhruv/Downloads/L_UNIQUE_CARRIERS.csv"
 	spark = SparkSession.builder.appName('Cleaning and Transforming Delay Dataset').getOrCreate()
 	main(inputs,inputs1)
-
-
