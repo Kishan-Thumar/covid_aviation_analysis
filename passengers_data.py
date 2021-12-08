6a606894a5a50d89015baeda9e8eb19d456d225f
krt4 <krt4@sfu.ca>
2021-12-07 18:12:28 -0800

    code cleaning & refactoring

--- passengers_data.py
+++ passengers_data.py
@@ -1,13 +1,9 @@
 import sys, re
-assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+assert sys.version_info >= (3, 5)
 from pyspark.sql import SparkSession, functions, types
-from pyspark.sql.functions import split
+from pyspark.sql.functions import split, when, lit, col
 
-# spark = SparkSession.builder.appName('passengers data').getOrCreate()
-# # Make sure we have Spark 3.0+
-# assert spark.version >= '3.0' 
-# spark.sparkContext.setLogLevel('WARN')
-
+# UDF function to get appropriate carrier name
 @functions.udf(returnType=types.StringType())
 def processed_file_name(flight):
     fka_re = " fka | f/k/a "
@@ -19,41 +15,34 @@ def processed_file_name(flight):
     else:
         return flight
     
-def fetch_passengers_data(spark,passengers,service_class,data_source):    
-    #path to respective files
-    #passengers = '/Users/himalyabachwani/Documents/Big_Data_732/project_data/849844085_T_T100_SEGMENT_ALL_CARRIER.csv'
-    #service_class = '/Users/himalyabachwani/Documents/Big_Data_732/project_data/Service_class.csv'
-    #data_source = '/Users/himalyabachwani/Documents/Big_Data_732/project_data/Data_source.csv'
+# Function to fetch data from S3, performing ETL and returning final_df.
+def fetch_passengers_data(spark,passengers):
+    # Fetching passenger data from S3 
+    passengers_data = spark.read.csv(passengers, header=True, inferSchema= True)\
+        .withColumn('UNIQUE_CARRIER_NAME',processed_file_name('UNIQUE_CARRIER_NAME'))\
+        .withColumn('CARRIER_ORIGIN',when(col('DATA_SOURCE') == "DU", lit("Domestic Data, US Carriers Only"))\
+        .when(col('DATA_SOURCE') == "DF", lit("Domestic Data, Foreign Carriers"))\
+        .when(col('DATA_SOURCE') == "IF", lit("International Data, Foreign Carriers"))\
+        .when(col('DATA_SOURCE') == "IU", lit("International Data, US Carriers Only")))
 
-    #passengers data
-    passengers_data = spark.read.csv(passengers, header=True, inferSchema= True).withColumn('UNIQUE_CARRIER_NAME',processed_file_name('UNIQUE_CARRIER_NAME'))
+    # Selecting revelant columns from passengers_data
+    processed_passengers_data = passengers_data.select('PAYLOAD','SEATS','PASSENGERS','FREIGHT','MAIL','DISTANCE','AIRLINE_ID','UNIQUE_CARRIER','UNIQUE_CARRIER_NAME','ORIGIN_AIRPORT_ID','ORIGIN','ORIGIN_STATE_ABR','ORIGIN_STATE_NM','DEST_AIRPORT_ID','DEST','DEST_STATE_ABR','DEST_STATE_NM','YEAR','MONTH','CLASS','DATA_SOURCE', 'CARRIER_ORIGIN')
 
-    #lookup tables
-    service_class_data = spark.read.csv(service_class, header=True, inferSchema= True)
-    data_source_data = spark.read.csv(data_source, header=True, inferSchema= True)
-
-    processed_passengers_data = passengers_data.select('PAYLOAD','SEATS','PASSENGERS','FREIGHT','MAIL','DISTANCE','AIRLINE_ID','UNIQUE_CARRIER','UNIQUE_CARRIER_NAME','ORIGIN_AIRPORT_ID','ORIGIN','ORIGIN_STATE_ABR','ORIGIN_STATE_NM','DEST_AIRPORT_ID','DEST','DEST_STATE_ABR','DEST_STATE_NM','YEAR','MONTH','CLASS','DATA_SOURCE').withColumn('CARRIER_NAME',processed_file_name('UNIQUE_CARRIER_NAME'))
-
+    # Creating views 
     processed_passengers_data.createOrReplaceTempView("processed_passengers_data")
-    service_class_data.createOrReplaceTempView("service_class_data")
-    data_source_data.createOrReplaceTempView("data_source_data")
-
-
+    
+    # Performing aggregation for 
     final_passengers_data = spark.sql("""
                             SELECT SUM(PD.PASSENGERS) AS Passengers, SUM(PD.SEATS) AS Seats,
                             AVG(PD.PASSENGERS / PD.SEATS) AS Occupancy_Ratio,
                             SUM(PD.FREIGHT) AS Freight, SUM(PD.MAIL) AS Mail,
                             AVG(PD.DISTANCE) AS Distance, PD.UNIQUE_CARRIER, PD.UNIQUE_CARRIER_NAME,
                             PD.ORIGIN_STATE_ABR, PD.ORIGIN_STATE_NM, PD.DEST_STATE_ABR, PD.DEST_STATE_NM,
-                            PD.YEAR, PD.MONTH, PD.DATA_SOURCE, DS.Description AS CARRIER_ORIGIN
+                            PD.YEAR, PD.MONTH, PD.DATA_SOURCE, PD.CARRIER_ORIGIN
                             FROM processed_passengers_data AS PD
-                            JOIN service_class_data AS SC
-                            ON PD.CLASS = SC.Code
-                            JOIN data_source_data AS DS
-                            ON PD.DATA_SOURCE = DS.Code
-                            WHERE DS.Code IN ('DU','DF')
+                            WHERE PD.DATA_SOURCE IN ('DU','DF')
                             GROUP BY PD.UNIQUE_CARRIER, PD.UNIQUE_CARRIER_NAME,
                             PD.ORIGIN_STATE_ABR, PD.ORIGIN_STATE_NM, PD.DEST_STATE_ABR, PD.DEST_STATE_NM,
-                            PD.YEAR, PD.MONTH, PD.DATA_SOURCE, DS.Description
+                            PD.YEAR, PD.MONTH, PD.DATA_SOURCE, PD.CARRIER_ORIGIN
     """)
     return final_passengers_data
